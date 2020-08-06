36fdf46bc0eafd2598e50ad16908916f4a6942e0
Fiza_khan <FIZA-KHAN@users.noreply.github.com>
2020-08-06 15:43:09 +0530

    Hackerrank-30-Days-of-Code

---
+++ Day_1_Data_Types.py
@@ -0,0 +1,15 @@
+
+# Declare second integer, double, and String variables.
+number_input = int(input())
+double_input = float(input())
+string_input = str(input())
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(i+number_input)
+# Print the sum of the double variables on a new line.
+dub = d + double_input
+print(round(dub, 1))
+# Concatenate and print the String variables on a new line
+print(s + string_input)
+# The 's' variable above should be printed first.
